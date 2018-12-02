0a9ec1e9617be2ce1808a47a003d843a89f032a1
Zero <wemersonjsa@gmail.com>
2018-12-02 14:35:51 -0300

    Adicionado novos arquivos ao repositório

---
+++ aula009.py
@@ -0,0 +1,7 @@
+idade = int(input('Digite sua idade: '))
+if idade >= 10 and idade < 20:
+    print ('Você é adolecente')
+elif idade >= 20 and idade < 30:
+    print ('Você é Jovem')
+else:
+    print('Valor não encontrado!')

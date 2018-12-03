23a6815d76541f07e66d73c0e07210db6d9eb33d
Zero <wemersonjsa@gmail.com>
2018-12-02 23:06:02 -0300

    Adicionado novos arquivos ao repositório

--- aula009.py
+++ aula009.py
@@ -1,7 +1,2 @@
-idade = int(input('Digite sua idade: '))
-if idade >= 10 and idade < 20:
-    print ('Você é adolecente')
-elif idade >= 20 and idade < 30:
-    print ('Você é Jovem')
-else:
-    print('Valor não encontrado!')
+frase = 'Curso em Video Python'
+print(frase[1:])
